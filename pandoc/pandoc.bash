6f7008013f792579ec8527f7e6d80f11f6806936
arnicas <arnicas@gmail.com>
2016-01-06 17:47:25 -0500

    Fix path to css

--- pandoc/pandoc.bash
+++ pandoc/pandoc.bash
@@ -4,7 +4,7 @@ for f in Week*/README.md
 do
  DIR="$( dirname "$f")";
  echo "$DIR";
- pandoc --from markdown_github --to html $f --toc -H pandoc/pandoc_header.txt -B pandoc/pandoc_before.txt > $DIR/index.html
+ pandoc --from markdown_github --to html $f --toc -H pandoc/pandoc_header_weeks.txt -B pandoc/pandoc_before.txt > $DIR/index.html
 done
 
 echo "Doing top level index and examples..."
